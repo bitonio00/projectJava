14f3d950afc59c84891e0cb926bd920a6eb76ac5
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-11-18 17:33:34 +0100

    Update Buyer.java

--- RealEstateProject/src/realestateproject/Buyer.java
+++ RealEstateProject/src/realestateproject/Buyer.java
@@ -18,6 +18,7 @@ public class Buyer extends Personne{
     boolean m_returning;
     private int m_login;
     private String m_password;
+    // jean michel groscul
     
     public Buyer(String nom, String prenom, int age, String adress)
     {
