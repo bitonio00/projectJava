6e7f2e8273944900967f35cf57291eb97365fa17
benoit <benoitmnt@gmail.com>
2020-11-19 21:59:50 +0100

    Update RealEstateProject.java

--- RealEstateProject/src/realestateproject/RealEstateProject.java
+++ RealEstateProject/src/realestateproject/RealEstateProject.java
@@ -15,7 +15,8 @@ public class RealEstateProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        RealEstateAgency a=new RealEstateAgency("pp","nn");
+        a.displayBuyer();
     }
     
 }
