a26a32abe4ac0a32492034f4b78ddf463021b0cb
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-11-19 19:25:24 +0100

    Update Offer.java

--- RealEstateProject/src/realestateproject/Offer.java
+++ RealEstateProject/src/realestateproject/Offer.java
@@ -10,15 +10,20 @@ package realestateproject;
  * @author benoi
  */
 public class Offer {
-    private long m_montant;
-    Estate m_estateconcerned;
-    Buyer m_buyer;
-    String m_type;
+    private double m_montant;
+   private Estate m_estateconcerned;
+   private Buyer m_buyer;
+   private String m_type; 
     
-   public Offer( long montant,  , Buyer buyer, String type)
+   public Offer( long montant,Buyer buyer,Estate estateconcerned,   String type)
    {
    m_montant= montant;
-   
+   m_type=type;
+   m_buyer= new Buyer( buyer.getNom(),buyer.getPrenom(),buyer.getDate().getYear(),buyer.getDate().getMonth(),buyer.getDate().getDay(),
+           buyer.getAdress().getCountry(), buyer.getAdress().getCity(),buyer.getAdress().getStreet());
+    
+   m_estateconcerned = new Estate( estateconcerned.getSize(),estateconcerned.getAdress().getCountry(),estateconcerned.getAdress().getCity(),
+            estateconcerned.getAdress().getStreet(), estateconcerned.getPrice());
    }// a voir avec benoit
            
            }
