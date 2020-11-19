7725e64ed80c1d153800a4810e14b55fb7396c53
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-11-19 10:23:01 +0100

    Update RealEstateAgency.java

--- RealEstateProject/src/realestateproject/RealEstateAgency.java
+++ RealEstateProject/src/realestateproject/RealEstateAgency.java
@@ -5,10 +5,18 @@
  */
 package realestateproject;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author benoi
  */
 public class RealEstateAgency {
-    
+    // arraylist de buyers, de seller ,dagent; doffre et de visite
+private String m_name;
+private ArrayList<Buyer>m_listBuyer;
+private ArrayList<Seller>m_listSeller  ;
+private ArrayList<Offer>m_listOffer;
+private ArrayList<RealEstateAgent>m_listRealEstateAgent;
+private ArrayList<Visit>m_listVisit;
 }
