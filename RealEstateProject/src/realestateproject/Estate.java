7deb71f608312375410a7768ae03d3595054629e
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-11-19 10:22:37 +0100

    Update Estate.java

--- RealEstateProject/src/realestateproject/Estate.java
+++ RealEstateProject/src/realestateproject/Estate.java
@@ -5,10 +5,29 @@
  */
 package realestateproject;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author benoi
  */
 public class Estate {
+    private int m_size;
+    private Adress m_adress;
+    private long m_price;
+    private ArrayList<Visit>m_listVisit;
+    private ArrayList<Offer>m_listOffer;
     
+public Estate(int size, Adress adress, long price)
+{
+   
+m_size=size;
+m_adress=adress;
+m_price=price;
+m_listVisit= new ArrayList<Visit>();
+m_listOffer= new ArrayList<Offer>();
+
+}
+
+
 }
