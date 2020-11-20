be7c0704b4d68a7b9b0ad6227b498adf73f89cf0
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-11-20 22:03:49 +0100

    Update RealEstateAgent.java

--- RealEstateProject/src/realestateproject/RealEstateAgent.java
+++ RealEstateProject/src/realestateproject/RealEstateAgent.java
@@ -15,13 +15,20 @@ public class RealEstateAgent extends Personne {
     private int m_login;
     private ArrayList<Estate> listEstate;
     private double m_commission;
+    private String m_password;
+  
 
-public RealEstateAgent(int year,int month, int day, String nom, String prenom , int login, double commission)
+public RealEstateAgent(int year,int month, int day, String nom, String prenom , int login, String password,double commission)
 {
     super(nom,prenom,year,month,day);
     m_login=login;
+    m_password=password;
+ 
     m_commission=commission;
     listEstate= new ArrayList<Estate>();
 }
-
+public void display()
+{
+    System.out.println("nom:"+getNom()+" prenom:"+getPrenom()+" born:"+getDate().getDate()+" login:"+m_login+" password:"+m_password);
+}
 }
