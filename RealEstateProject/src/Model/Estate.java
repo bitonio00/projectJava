c8876d1c2aa394c493d9ef6a1d691f8b709878ed
benoit <benoitmnt@gmail.com>
2020-11-20 22:30:46 +0100

    Create Estate.java

---
+++ RealEstateProject/src/Model/Estate.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author benoi
+ */
+public class Estate {
+    private int m_size;
+    private Adress m_adress;
+    private double m_price;
+ 
+   
+    
+public Estate(int size,String country,String city, String street , double price)
+{
+   
+m_size=size;
+
+m_price=price;
+
+m_adress= new Adress(country,city,street);
+}
+public int getSize()
+{
+return m_size;
+}
+
+public double getPrice()
+{
+return m_price;
+}
+public Adress getAdress()
+{
+return m_adress;
+}
+
+}
