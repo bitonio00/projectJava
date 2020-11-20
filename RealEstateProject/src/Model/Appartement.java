5f4c9f589268949db425f9c73640387c3b8bcfde
benoit <benoitmnt@gmail.com>
2020-11-20 22:30:59 +0100

    Create Appartement.java

---
+++ RealEstateProject/src/Model/Appartement.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author benoi
+ */
+public class Appartement    {
+    private int m_numberofloor;
+    private int m_numberofroom;
+    private int m_numberofbedroom;
+    private int m_numberofbathroom;
+    private boolean meubled;
+    private boolean terace;
+    private boolean visavis;
+    private boolean garden;
+    // a voir avec benoit
+}
