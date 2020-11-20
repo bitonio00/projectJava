cb24fae0f458cf1c60d9c22be6ac38ee07ea9a2f
benoit <benoitmnt@gmail.com>
2020-11-20 22:31:03 +0100

    Create Adress.java

---
+++ RealEstateProject/src/Model/Adress.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author benoi
+ */
+public class Adress {
+   private String m_country;
+      private String m_city;
+       private String m_street;
+    
+    public Adress( String country ,String city, String street) 
+    {
+        m_country=country;
+        m_city=city;
+         m_street=street;
+        
+        
+    }
+    public String toString()
+    {
+        return m_country+" "+m_city+" "+m_street;
+    }
+    public String getCountry()
+    {
+    return m_country;
+
+    }
+    public String getCity()
+    {
+    return m_city;
+    }
+    public String getStreet()
+    {
+    return m_street;
+
+    }
+}
