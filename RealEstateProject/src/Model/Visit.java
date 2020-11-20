1efad88da4b3c3de36cc8d13272dde18892a20b2
benoit <benoitmnt@gmail.com>
2020-11-20 22:30:20 +0100

    Create Visit.java

---
+++ RealEstateProject/src/Model/Visit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author benoi
+ */
+public class Visit {
+    private Estate m_estateviewed;
+    private Buyer m_estatebuyer;
+    private RealEstateAgent m_estateagent;
+    // acompleter avec benoit
+}
