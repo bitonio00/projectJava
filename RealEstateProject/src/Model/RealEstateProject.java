cb85f5a1994e481f527bf31791d6aa499a4e14d1
benoit <benoitmnt@gmail.com>
2020-11-20 22:30:27 +0100

    Create RealEstateProject.java

---
+++ RealEstateProject/src/Model/RealEstateProject.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+import View.*;
+
+/**
+ *
+ * @author benoi
+ */
+public class RealEstateProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RealEstateAgency a=new RealEstateAgency("pp","nn");
+        a.displayBuyer();
+        a.interact();
+        a.displayOffer();
+        a.delateBuyer(1001);
+        RealEstateAgency b=new RealEstateAgency("pp","nn");
+        b.displayBuyer();
+        LoginPage c=new LoginPage();
+        c.LoadLoginPage();
+        
+    }
+    
+}
