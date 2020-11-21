f9401fd19022b363c31dc33e41305123ab549771
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-11-21 22:05:55 +0100

    Update Seller.java

--- RealEstateProject/src/Model/Seller.java
+++ RealEstateProject/src/Model/Seller.java
@@ -4,7 +4,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- package Model;
+ 
+
+package Model;
 
  import java.util.ArrayList;
 
@@ -13,19 +15,19 @@
  * @author benoi
  */
 public class Seller extends Personne {
-    private int m_login;
-    private String m_password;
-    private Adress m_adress;
+    
+    
+   
     private ArrayList<Estate>m_listEstate;
     public Seller(String nom, String prenom,int year,int month,int day,   String country,String city,String street ,int login, String password)
     {
-        super(nom,prenom,year,month,day);
-        m_login=login;
-        m_adress= new Adress(country,city,street);
-        m_password=password;
+        super(nom,prenom,year,month,day,login,password,country,city,street);
+        
+        
+       
     }
    public void display()
     {
-    System.out.println("nom:"+getNom()+" prenom:"+getPrenom()+" born:"+getDate().getDate()+" adress:"+m_adress.toString()+" login:"+m_login+" password:"+m_password);
+    System.out.println("nom:"+getNom()+" prenom:"+getPrenom()+" born:"+getDate().getDate()+" adress:"+getAdress().toString()+" login:"+getLogin()+" password:"+getPassword());
     }
 }
