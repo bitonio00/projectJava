c73ea334e3759e42904659ada9dcaf90217bf6e4
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-11-21 22:06:25 +0100

    Create House.java

---
+++ RealEstateProject/src/Model/House.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author moule
+ */
+public class House extends Estate {
+    private String m_houseType;
+    private boolean m_garden;
+    
+   
+
+    public House(int size,String country,String city, String street , double price,Seller seller, RealEstateAgent realEstateAgent, String houseType, int numberOfFloor, boolean meubled,boolean equiped,boolean garden)
+    {
+    super(size,country,city,street,price,seller,realEstateAgent,numberOfFloor,equiped,meubled);
+    m_houseType=  houseType;
+   m_garden=garden;
+    
+    }
+
+public String getHouseType()
+{
+    return m_houseType;
+}
+
+public boolean getGarden()
+{
+return m_garden;
+}
+
+
+
+}
