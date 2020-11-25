62c840f822bc0b6939a886f9c8e0fcfb7bef4e1c
benoit <benoitmnt@gmail.com>
2020-11-25 20:31:49 +0100

    Update TypePage.java

--- RealEstateProject/src/View/TypePage.java
+++ RealEstateProject/src/View/TypePage.java
@@ -5,24 +5,61 @@
  */
 package View;
 
+import Model.Buyer;
+import Model.RealEstateAgent;
+import Model.Seller;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.*;
 
 /**
  *
  * @author benoi
  */
-public class TypePage  implements ActionListener  {
+public class TypePage extends MasterList implements ActionListener  {
     private static JPanel panel;
     private static JFrame frame;
     private static JLabel textLabel;
     private static JButton button1;
     private static JButton button2;
     private static JButton button3;
-    private String type;
+    private static JButton button4;
+    private static String type;
+    /*private static ArrayList<Buyer>listB;
+    private static ArrayList<Seller>listS;
+    private static ArrayList<RealEstateAgent>listREA;*/
     
-    public void LoadLoginPage ()
+    /*public void setList(ArrayList<Buyer>listBuyer,ArrayList<Seller>listSeller,ArrayList<RealEstateAgent>listRealEstateAgent)
+               {
+        listB=new ArrayList<Buyer>();
+        for(int i=0;i<listBuyer.size(); ++i)
+        {
+            //String nom, String prenom, int year,int month,int day, String country,String city,String street,int login,String password
+            listB.add(new Buyer(listBuyer.get(i).getNom(),listBuyer.get(i).getPrenom(), listBuyer.get(i).getDate().getYear(), 
+             listBuyer.get(i).getDate().getMonth(), listBuyer.get(i).getDate().getDay(),  listBuyer.get(i).getAdress().getCountry(),
+             listBuyer.get(i).getAdress().getCity(), listBuyer.get(i).getAdress().getStreet(), listBuyer.get(i).getLogin(), listBuyer.get(i).getPassword()));
+        }
+        listS=new ArrayList<Seller>();
+        for(int i=0;i<listSeller.size(); ++i)
+        {
+           // String nom, String prenom,int year,int month,int day,   String country,String city,String street ,int login, String password
+            listS.add(new Seller(listSeller.get(i).getNom(),listSeller.get(i).getPrenom(), listSeller.get(i).getDate().getYear(), 
+             listSeller.get(i).getDate().getMonth(), listSeller.get(i).getDate().getDay(),  listSeller.get(i).getAdress().getCountry(),
+             listSeller.get(i).getAdress().getCity(), listSeller.get(i).getAdress().getStreet(), listSeller.get(i).getLogin(), listSeller.get(i).getPassword()));
+        }
+        listREA=new ArrayList<RealEstateAgent>();
+        for(int i=0;i<listRealEstateAgent.size(); ++i)
+        {
+            //(int year,int month, int day, String nom, String prenom , int login, String password,double commission,String country,String city,String street
+        
+            listREA.add(new RealEstateAgent(listRealEstateAgent.get(i).getDate().getYear(),listRealEstateAgent.get(i).getDate().getMonth(),listRealEstateAgent.get(i).getDate().getDay(), 
+                    listRealEstateAgent.get(i).getNom(),listRealEstateAgent.get(i).getPrenom(),listRealEstateAgent.get(i).getLogin(),listRealEstateAgent.get(i).getPassword(), listRealEstateAgent.get(i).getCommission(),
+                    listRealEstateAgent.get(i).getAdress().getCountry(), listRealEstateAgent.get(i).getAdress().getCity(), listRealEstateAgent.get(i).getAdress().getStreet()));
+        }
+            }*/
+    public void LoadTypePage ()
     {
        
         
@@ -44,18 +81,27 @@ public class TypePage  implements ActionListener  {
         
         button1=new JButton("Buyer");
         button1.setBounds(10, 80, 80, 25);
+        button1.setSize(new Dimension(200, 40));
         panel.add(button1);
         button1.addActionListener(new TypePage());
         
         button2=new JButton("Seller");
-        button2.setBounds(120, 80, 80, 25);
+        button2.setBounds(300, 80, 80, 25);
+        button2.setSize(new Dimension(200, 40));
         panel.add(button2);
         button2.addActionListener(new TypePage());
         
-        button3=new JButton("REA");
-        button3.setBounds(260, 80, 80, 25);
+        button3=new JButton("Real Estate Agent");
+        button3.setBounds(590, 80, 80, 25);
+        button3.setSize(new Dimension(200, 40));
         panel.add(button3);
         button3.addActionListener(new TypePage());
+        
+        button4=new JButton("New Customer");
+        button4.setBounds(880, 80, 80, 25);
+        button4.setSize(new Dimension(200, 40));
+        panel.add(button4);
+        button4.addActionListener(new TypePage());
           
         
         frame.setVisible(true);
@@ -70,11 +116,23 @@ public class TypePage  implements ActionListener  {
     @Override
     public void actionPerformed(ActionEvent ae) {
       type=ae.getActionCommand();
-      System.out.println(type);
-      frame.setVisible(false);
+      RegisterPage b=null;
+     // frame.setVisible(false);
+      if(type.equals("New Customer"))
+      {
+          b=new RegisterPage();
+          b.loadRegisterPage();
+      }
+      else
+      {
+      
       LoginPage a=new LoginPage();
       a.setLoginPage(type);
-      a.LoadLoginPage();
+      a.LoadLoginPage();   
+      }
+      frame.setVisible(false);
+      frame.dispose();
+      
     }
     
 }
