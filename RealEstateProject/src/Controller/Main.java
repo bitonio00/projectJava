b52973e994713cdec054cd88fa0aeb4da082e837
benoit <benoitmnt@gmail.com>
2020-11-26 21:02:34 +0100

    Create Main.java

---
+++ RealEstateProject/src/Controller/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.MasterList;
+
+/**
+ *
+ * @author benoi
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+
+//        a.interact();
+     //   a.displayOffer();
+     //   a.delateBuyer(1001);
+        //RealEstateAgency b=new RealEstateAgency("pp","nn");
+        App a=new App();
+        //a.display();
+        MasterList m=new MasterList();
+        m.setList(a.getB(),a.getS(),a.getR(),a.getE());
+        m.loadGui();
+        
+        //MasterList a=new MasterList();
+       // a.setList(b.getBuyer(),b.getSeller(),b.getREA(),b.getEstate());
+        //a.loadGui();
+        //BuyerPage c=new BuyerPage();
+        //c.loadBuyerPage();
+       //BuyerPage c=new BuyerPage();
+       //c.loadBuyerPage();
+
+       
+    }
+    
+}
