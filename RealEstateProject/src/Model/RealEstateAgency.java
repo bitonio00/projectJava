2a0f4f4b29fdaeb325bd7bf58215f0813ab5d62b
benoit <benoitmnt@gmail.com>
2020-11-26 21:02:32 +0100

    Update RealEstateAgency.java

--- RealEstateProject/src/Model/RealEstateAgency.java
+++ RealEstateProject/src/Model/RealEstateAgency.java
@@ -57,10 +57,7 @@ public void delateBuyer(int val)
 
 public void displayBuyer()
 {
-    for(int i=0; i<m_listBuyer.size();++i)
-    {
-        m_listBuyer.get(i).display();
-    }
+   
 }
 public int findBuyer(int id)
 {
@@ -84,10 +81,7 @@ public void readSeller()
 
 public void displaySeller()
 {
-    for(int i=0; i<m_listSeller.size();++i)
-    {
-        m_listSeller.get(i).display();
-    }
+    
 }
 
 public int findSeller(int id)
@@ -110,11 +104,7 @@ public void readRealEstateAgent()
 
 public void displayRealEstateAgent()
 {
-    for(int i=0; i<m_listRealEstateAgent.size();++i)
-    {
-
-        m_listRealEstateAgent.get(i).display();
-    }
+    
 }
 public int findEstateAgent(int id)
 {
@@ -134,10 +124,7 @@ public int findEstateAgent(int id)
 
 public void displayOffer()
 {
-  for(int i=0; i<m_listOffer.size();++i)
-    {
-        m_listOffer.get(i).display();
-    }
+  
 }
 public void interact()
 {
@@ -152,10 +139,7 @@ public void readEstate()
 
 public void displayEstate()
 {
-  for(int i=0; i<m_listEstate.size();++i)
-    {
-        m_listEstate.get(i).display();
-    }
+  
 }
 
 public int findEstate(int id)
@@ -175,66 +159,7 @@ public int findEstate(int id)
 public void readOffer()
 {
 
-    Connection conn=null;
-        try
-        {
-
-            String url="jdbc:mysql://localhost:3306/estate_agency";
-            String user="root";
-            String password="";
-
-            conn=DriverManager.getConnection(url,user,password);
-
-            Statement stmt=conn.createStatement();
-            //String nom, String prenom, int year,int month,int day, String country,String city,String street,int login,String Password)
-            ResultSet rs=stmt.executeQuery("select * from offer ");// c pa une faute tu la appelr comme sa sur php
-
-            while(rs.next())
-            {
-                int indiceb=findBuyer(rs.getInt(2));
-                int indicee= findEstate(rs.getInt(3));
-               if (m_listEstate.get(indicee).getType().equals("appartement"))
-               {//public Offer( int id,double montant,Buyer buyer,House  houseconcerned,   String type)
-               Appartement a= new Appartement(m_listEstate.get(indicee).getId(),m_listEstate.get(indicee).getSize(),
-                       m_listEstate.get(indicee).getAdress().getCountry(),m_listEstate.get(indicee).getAdress().getCity(),
-                       m_listEstate.get(indicee).getAdress().getStreet(),m_listEstate.get(indicee).getPrice(),m_listEstate.get(indicee).getSeller()
-               ,m_listEstate.get(indicee).getAgent(),m_listEstate.get(indicee).getNOF(),m_listEstate.get(indicee).getEquiped(),
-                       m_listEstate.get(indicee).getMeubled(),m_listEstate.get(indicee).getvisavis(),m_listEstate.get(indicee).getType(),
-               m_listEstate.get(indicee).getNor(),m_listEstate.get(indicee).getNob());
-
-                  m_listOffer.add(new Offer(rs.getInt(1),rs.getDouble(4), m_listBuyer.get(indiceb),a,rs.getString(5)));
-               }
-               if (m_listEstate.get(indicee).getType().equals("house"))
-               {//public Offer( int id,double montant,Buyer buyer,House  houseconcerned,   String type)
-               House a= new House(m_listEstate.get(indicee).getId(),m_listEstate.get(indicee).getSize(),
-                       m_listEstate.get(indicee).getAdress().getCountry(),m_listEstate.get(indicee).getAdress().getCity(),
-                       m_listEstate.get(indicee).getAdress().getStreet(),m_listEstate.get(indicee).getPrice(),m_listEstate.get(indicee).getSeller()
-               ,m_listEstate.get(indicee).getAgent(),m_listEstate.get(indicee).getHouseType(),m_listEstate.get(indicee).getNOF(),
-                       m_listEstate.get(indicee).getMeubled(),m_listEstate.get(indicee).getEquiped(), m_listEstate.get(indicee).getGarden(),
-                       m_listEstate.get(indicee).getType(),m_listEstate.get(indicee).getNor(),m_listEstate.get(indicee).getNob());
-
-
-                  m_listOffer.add(new Offer(rs.getInt(1),rs.getDouble(4), m_listBuyer.get(indiceb),a,rs.getString(5)));
-               }
-                if (m_listEstate.get(indicee).getType().equals("local"))
-               {
-               Local a= new Local(m_listEstate.get(indicee).getId(),m_listEstate.get(indicee).getSize(),
-                       m_listEstate.get(indicee).getAdress().getCountry(),m_listEstate.get(indicee).getAdress().getCity(),
-                       m_listEstate.get(indicee).getAdress().getStreet(),m_listEstate.get(indicee).getPrice(),m_listEstate.get(indicee).getSeller()
-               ,m_listEstate.get(indicee).getAgent(),m_listEstate.get(indicee).getLocalType(),
-                       m_listEstate.get(indicee).getEquiped(),m_listEstate.get(indicee).getMeubled(), m_listEstate.get(indicee).getNOF(),m_listEstate.get(indicee).getType());
-
-/*public Local(int id,int size,String country,String city, String street , double price,Seller seller,
-        RealEstateAgent realEstateAgent, String localType,boolean equiped, boolean meubled, int numberOfFloor, String type)*/
-                  m_listOffer.add(new Offer(rs.getInt(1),rs.getDouble(4), m_listBuyer.get(indiceb),a,rs.getString(5)));
-               }
-            }
-            conn.close();
-        }
-        catch(SQLException e)
-        {
-            System.out.println(""+e.getMessage());
-        }
+              
 }
 public void readVisit()
 {
@@ -245,10 +170,7 @@ public void displayVisit()
 {
 
 
-  for(int i=0; i<m_listVisit.size();++i)
-    {
-        m_listVisit.get(i).display();
-    }
+  
 
   
 
