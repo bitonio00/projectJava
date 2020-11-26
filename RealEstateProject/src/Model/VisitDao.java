34683a82d04a27d88140576b1eba9bcfc5403c08
benoit <benoitmnt@gmail.com>
2020-11-26 17:39:20 +0100

    Create VisitDao.java

---
+++ RealEstateProject/src/Model/VisitDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author benoi
+ */
+public interface VisitDao {
+    ArrayList<Visit>readVisit(ArrayList<Buyer>listBuyer,ArrayList<Estate>listEstate,ArrayList<RealEstateAgent>listRealEsateAgent);
+    
+}
