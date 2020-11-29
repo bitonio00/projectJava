63c75407032e276cc1a10670d7043e86d63a1939
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-11-29 04:10:56 +0100

    Update TypePage.java

--- RealEstateProject/src/View/TypePage.java
+++ RealEstateProject/src/View/TypePage.java
@@ -8,10 +8,16 @@ package View;
 import Model.Buyer;
 import Model.RealEstateAgent;
 import Model.Seller;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
@@ -22,6 +28,7 @@ public class TypePage extends MasterList implements ActionListener  {
     private static JPanel panel;
     private static JFrame frame;
     private static JLabel textLabel;
+    private static JLabel image;
     private static JButton button1;
     private static JButton button2;
     private static JButton button3;
@@ -59,7 +66,7 @@ public class TypePage extends MasterList implements ActionListener  {
                     listRealEstateAgent.get(i).getAdress().getCountry(), listRealEstateAgent.get(i).getAdress().getCity(), listRealEstateAgent.get(i).getAdress().getStreet()));
         }
             }*/
-    public void LoadTypePage ()
+    public void LoadTypePage () throws IOException
     {
        
         
@@ -67,41 +74,85 @@ public class TypePage extends MasterList implements ActionListener  {
        
         panel=new JPanel();
         frame= new JFrame();
-        frame.setSize(4000, 4000);
+        frame.setSize(2000, 2000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         frame.add(panel);
         
+        
+       
+         //ImageIcon icon = new ImageIcon("imtest.jpg");
+         //rame.add(new JLabel(icon));
+        // frame.pack();
+    
+    
+        
+       
         panel.setLayout(null);
+        JLabel background1 = new JLabel(new ImageIcon("fondp1.png"));
+        background1.setBounds(0, 0, 80, 80);
+        
+        
+        frame.add(background1); 
+        frame.pack();
+        frame.setResizable(true);
+        frame.add(panel);
+           
+        //image =new JLabel("background.png");
+        //image.setBounds(0, 0, 4000, 4000);
+       // panel.add(image);
+        JLabel fondL= new JLabel("CasaDelSol");
+        fondL.setBounds(820, 450, 1000, 200);
+        fondL.setForeground(Color.blue);
+        
+        fondL.setFont(new Font("Verdera", Font.PLAIN,45));
+        
+        panel.add(fondL);
+        
         textLabel=new JLabel("Connect As");
-        textLabel.setBounds(850,200,400,100);
+        textLabel.setBounds(850,600,200,200);
+        textLabel.setForeground(Color.blue);
+        textLabel.setFont(new Font("Verdera", Font.PLAIN,35));
         
         panel.add(textLabel);
+        
+         
+        //BufferedImage img = ImageIO.read(new File("test.png"));
+       // JLabel pic = new JLabel(new ImageIcon(img));
+        //panel.add(pic);
+        //frame.add(panel);
+        //frame.setSize(400, 400);
+   
        
         
         button1=new JButton("Buyer");
-        button1.setBounds(200, 500, 400, 100);
-        button1.setSize(new Dimension(200, 40));
+        button1.setBounds(200, 800, 400, 100);
+       button1.setForeground(Color.black);
+        button1.setBackground(Color.pink);
         panel.add(button1);
         button1.addActionListener(new TypePage());
         
         button2=new JButton("Seller");
-        button2.setBounds(600, 500, 400, 100);
-        button2.setSize(new Dimension(200, 40));
+        button2.setBounds(600, 800, 400, 100);
+         button2.setForeground(Color.black);
+        button2.setBackground(Color.pink);
         panel.add(button2);
         button2.addActionListener(new TypePage());
         
         button3=new JButton("Real Estate Agent");
-        button3.setBounds(1000, 500, 400, 100);
-        button3.setSize(new Dimension(200, 40));
+        button3.setBounds(1000, 800, 400, 100);
+        button3.setForeground(Color.black);
+        button3.setBackground(Color.pink);
         panel.add(button3);
         button3.addActionListener(new TypePage());
         
         button4=new JButton("New Customer");
-        button4.setBounds(1400, 500, 400, 100);
-        button4.setSize(new Dimension(400, 100));
+        button4.setBounds(1400, 800, 400, 100);
+        //button4.setSize(new Dimension(400, 100));
+        button4.setForeground(Color.black);
+        button4.setBackground(Color.pink);
         panel.add(button4);
-        button4.addActionListener(new TypePage());
+          button4.addActionListener(new TypePage());
           
         
         frame.setVisible(true);
@@ -136,3 +187,35 @@ public class TypePage extends MasterList implements ActionListener  {
     }
     
 }
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moule
+ */
+
+  
+    
+ /*   ImagePanel() 
+ {
+  try 
+  {
+   JFrame f = new JFrame("Ajouter une image dans JPanel");
+   JPanel panel = new JPanel();
+   panel.setBounds(50, 50, 250, 250);
+   BufferedImage img = ImageIO.read(new File("test.png"));
+   JLabel pic = new JLabel(new ImageIcon(img));
+   panel.add(pic);
+   f.add(panel);
+   f.setSize(400, 400);
+   f.setLayout(null);
+   f.setVisible(true);
+  } 
+  catch (IOException e) {}
+ }
+    
+}*/
