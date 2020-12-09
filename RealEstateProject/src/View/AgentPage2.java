025721a564bec3cbf0c7f6a5cb50095e5bb5c7a2
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-12-09 15:11:58 +0100

    Update AgentPage2.java

--- RealEstateProject/src/View/AgentPage2.java
+++ RealEstateProject/src/View/AgentPage2.java
@@ -39,6 +39,7 @@ public class AgentPage2 extends MasterList implements ActionListener{
     private static JComboBox formList;
     private static JComboBox sellerList;
     private static JButton button1;
+    private static JButton returnButton;
     private static JCheckBox jardinCheck;
     private static JCheckBox visavisCheck;
     private static JCheckBox equipedCheck;
@@ -81,7 +82,7 @@ public class AgentPage2 extends MasterList implements ActionListener{
         String[] formStrings = {"appartement", "house", "manoir", "villa", "local", "local_pro", "local_perso"};
         formList = new JComboBox(formStrings);
         formList.setSelectedIndex(1);
-        formList.setBounds(700, 450, 100, 50);
+        formList.setBounds(700, 550, 100, 50);
         formList.addActionListener(new actionComboType());
         panel.add(formList);
         String [] sellerStrings;
@@ -115,105 +116,111 @@ public class AgentPage2 extends MasterList implements ActionListener{
         
         sellerList = new JComboBox(sellerStrings);
         sellerList.setSelectedIndex(0);
-        sellerList.setBounds(900, 450, 100, 50);
+        sellerList.setBounds(900, 550, 100, 50);
         //reaList.setSize(550, 30);
         sellerList.addActionListener(new ActionComboSeller());
         panel.add(sellerList);
 
         priceLabel = new JLabel("PRICE:");
-        priceLabel.setBounds(700, 550, 100, 50);
+        priceLabel.setBounds(700, 650, 100, 50);
         priceText = new JTextField(15);
-        priceText.setBounds(800, 550, 100, 50);
+        priceText.setBounds(800, 650, 100, 50);
         panel.add(priceLabel);
         panel.add(priceText);
         
         sizeLabel = new JLabel("SIZE:");
-        sizeLabel.setBounds(900, 550, 100, 50);
+        sizeLabel.setBounds(900, 650, 100, 50);
         sizeText = new JTextField(15);
-        sizeText.setBounds(1000, 550, 100, 50);
+        sizeText.setBounds(1000, 650, 100, 50);
         panel.add(sizeLabel);
         panel.add(sizeText);
         
         
         countryLabel = new JLabel("Country:");
-        countryLabel.setBounds(600, 650, 100, 50);
+        countryLabel.setBounds(600, 750, 100, 50);
         countryText = new JTextField(15);
-        countryText.setBounds(700, 650, 100, 50);
+        countryText.setBounds(700, 750, 100, 50);
         panel.add(countryLabel);
         panel.add(countryText);
 
         cityLabel = new JLabel("City:");
-        cityLabel.setBounds(850, 650, 100, 50);
+        cityLabel.setBounds(850, 750, 100, 50);
         cityText = new JTextField(15);
-        cityText.setBounds(950, 650, 100, 50);
+        cityText.setBounds(950, 750, 100, 50);
         panel.add(cityLabel);
         panel.add(cityText);
 
         streetLabel = new JLabel("Street:");
-        streetLabel.setBounds(1100, 650, 100, 50);
+        streetLabel.setBounds(1100, 750, 100, 50);
         streetText = new JTextField(15);
-        streetText.setBounds(1200, 650, 100, 50);
+        streetText.setBounds(1200, 750, 100, 50);
         panel.add(streetLabel);
         panel.add(streetText);
 
         jardinCheck = new JCheckBox("Garden");
         jardinCheck.setSelected(false);
-        jardinCheck.setBounds(1380, 650, 100, 50);
+        jardinCheck.setBounds(1380, 750, 100, 50);
         jardinCheck.addItemListener(new CheckBox());
         panel.add(jardinCheck);
 
         visavisCheck = new JCheckBox("VisaVis");
         visavisCheck.setSelected(false);
-        visavisCheck.setBounds(1450, 650, 100, 50);
+        visavisCheck.setBounds(1450, 750, 100, 50);
         visavisCheck.addItemListener(new CheckBox());
         panel.add(visavisCheck);
 
         equipedCheck = new JCheckBox("equiped");
         equipedCheck.setSelected(false);
-        equipedCheck.setBounds(1550, 650, 100, 50);
+        equipedCheck.setBounds(1550, 750, 100, 50);
         equipedCheck.addItemListener(new CheckBox());
         panel.add(equipedCheck);
 
         meubledCheck = new JCheckBox("meubled");
         meubledCheck.setSelected(false);
-        meubledCheck.setBounds(1650, 650, 100, 50);
+        meubledCheck.setBounds(1650, 750, 100, 50);
         meubledCheck.addItemListener(new CheckBox());
         panel.add(meubledCheck);
 
         nbrFloorLabel = new JLabel("nbrFloor:");
-        nbrFloorLabel.setBounds(550, 750, 100, 50);
+        nbrFloorLabel.setBounds(550, 850, 100, 50);
         String[] numberFloorStrings = {"0", "1", "2", "3", "4", "5"};
         nbrFloorList = new JComboBox(numberFloorStrings);
         nbrFloorList.setSelectedIndex(0);
-        nbrFloorList.setBounds(700, 750, 100, 50);
+        nbrFloorList.setBounds(700, 850, 100, 50);
         nbrFloorList.addActionListener(new ActionComboFloor());
         panel.add(nbrFloorLabel);
         panel.add(nbrFloorList);
 
         nbrBedRoomLabel = new JLabel("nbrBedRoom:");
-        nbrBedRoomLabel.setBounds(850, 750, 100, 50);
+        nbrBedRoomLabel.setBounds(850, 850, 100, 50);
         String[] nbrBedRoomStrings = {"1", "2", "3", "4", "5", "6", "7", "8"};
         nbrBedRoomList = new JComboBox(nbrBedRoomStrings);
         nbrBedRoomList.setSelectedIndex(0);
-        nbrBedRoomList.setBounds(1000, 750, 100, 50);
+        nbrBedRoomList.setBounds(1000, 850, 100, 50);
         nbrBedRoomList.addActionListener(new ActionComboBedRoom());
         panel.add(nbrBedRoomLabel);
         panel.add(nbrBedRoomList);
 
         nbrBathRoomLabel = new JLabel("nbrBathRoom:");
-        nbrBathRoomLabel.setBounds(1150, 750, 100, 50);
+        nbrBathRoomLabel.setBounds(1150, 850, 100, 50);
         String[] nbrBathRoomStrings = {"1", "2", "3", "4", "5", "6", "7", "8"};
         nbrBathRoomList = new JComboBox(nbrBathRoomStrings);
         nbrBathRoomList.setSelectedIndex(0);
-        nbrBathRoomList.setBounds(1300, 750, 100, 50);
+        nbrBathRoomList.setBounds(1300, 850, 100, 50);
         nbrBathRoomList.addActionListener(new ActionComboBathRoom());
         panel.add(nbrBathRoomLabel);
         panel.add(nbrBathRoomList);
         
         button1 = new JButton("SUBMIT");
-        button1.setBounds(1000, 850, 200, 50);
+        button1.setBounds(850, 950, 200, 50);
         panel.add(button1);
         button1.addActionListener(new AgentPage2());
+        
+        returnButton=new JButton("return");
+        returnButton.setBounds(650, 950, 100, 50);
+        panel.add(returnButton);
+        returnButton.addActionListener(new AgentPage2.ActionReturn());
+
         
         frame.setVisible(true);
     }
@@ -270,14 +277,41 @@ public class AgentPage2 extends MasterList implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-      
+        boolean error=false;
         String priceS=priceText.getText();
         String sizeS=sizeText.getText();
         String country=countryText.getText();
+        String street= streetText.getText();
+        
+          if(country.contains("0")==true || country.contains("1")==true || country.contains("2")==true || country.contains("3")==true ||
+                 country.contains("4")==true || country.contains("5")==true || country.contains("6")==true || country.contains("7")==true ||
+                 country.contains("8")==true ||country.contains("9")==true ||country.equals("") || country.length()>25)
+         {
+          error=true;
+            countryText.setForeground(Color.red);
+         }else
+             countryText.setForeground(Color.black);
         String city=cityText.getText();
-        String street=streetText.getText();
+          if(city.contains("0")==true || city.contains("1")==true || city.contains("2")==true || city.contains("3")==true ||
+                 city.contains("4")==true || city.contains("5")==true || city.contains("6")==true || city.contains("7")==true ||
+                 city.contains("8")==true ||city.contains("9")==true ||city.equals("") || city.length()>25)
+         {
+          error=true;
+            cityText.setForeground(Color.red);
+         }else
+             cityText.setForeground(Color.black);
+          if( street.equals("") || street.length()>25)
+         {
+          error=true;
+            streetText.setForeground(Color.red);
+           //streetLabel.setBackground(Color.red);
+         }else
+             streetText.setForeground(Color.black);
+          
         int size=parseInt(sizeS);
         double price=parseDouble(priceS);
+       if (error==false)
+       {
         EstateDaoImpl estateDao=new EstateDaoImpl();
         if(typeEstate.equals("appartement"))
         {
@@ -321,6 +355,7 @@ public class AgentPage2 extends MasterList implements ActionListener{
         
         
         
+       }
     }
 
     
@@ -380,6 +415,21 @@ public class AgentPage2 extends MasterList implements ActionListener{
             typeEstate = (String) cb.getSelectedItem();
 
         }
+    }
+    
+    
+        public class ActionReturn implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            frame.setVisible(false);
+            frame.dispose();
+            AgentPage agentpage=new AgentPage ();
+         agentpage.loadAgentPage();
+
+        }
+
     }
     
 }
