0f8152cb5529cdbc4d2924829fa4fc9a0512ef04
mikasa-ackerman213 <moulekaf@hotmail.com>
2020-12-14 16:16:40 +0100

    Update Main.java

--- RealEstateProject/src/Controller/Main.java
+++ RealEstateProject/src/Controller/Main.java
@@ -39,43 +39,7 @@ public class Main {
         MasterList m=new MasterList();
         m.setList(a.getB(),a.getS(),a.getR(),a.getE(),a.getO(),a.getV());
        m.loadGui();
-       // SellerPage2 h= new  SellerPage2();
-       // h.loadSellerPage2();
-       //BuyerPage w = new BuyerPage();
-        //w.loadBuyerPage();
-     //TypePage h= new TypePage();
-    // h.LoadTypePage();
-        //BuyerPage b=new BuyerPage();
-        //b.loadBuyerPage();
-        //MasterList a=new MasterList();
-       // a.setList(b.getBuyer(),b.getSeller(),b.getREA(),b.getEstate());
-        //a.loadGui();
-        //BuyerPage c=new BuyerPage();
-        //c.loadBuyerPage();
-       //BuyerPage c=new BuyerPage();
-       //c.loadBuyerPage();
-      // AgentPage b= new AgentPage();
-       //b.loadAgentPage();
-//AgentPage2 c= new AgentPage2();
-//c.loadAgentPage2();
-//AgentPageAddVisit d= new AgentPageAddVisit();
-//d.loadAgentPageAddVisit();
-//SellerPage e = new SellerPage();
-//e.loadSellerPage();
-//SellerPage2 f= new SellerPage2();
-// nf.loadSellerPage2();
-//SellerPageAddVisit g= new SellerPageAddVisit();
-//g.loadSellerPageAddVisit();
-BuyerPage q =new BuyerPage();
-//q.loadBuyerPage();
-BuyerPage0 s= new BuyerPage0();
-//s.loadBuyerPage0();
-BuyerPage2 w= new BuyerPage2();
-//w.loadBuyerPage2();
-BuyerPage2Offer n= new BuyerPage2Offer();
-//n.loadBuyerPage2Offer();
-BuyerPage2Visit k = new BuyerPage2Visit();
-//k.loadBuyerPage2Visit();
+      
 
 
     }
